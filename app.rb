431aadba061d299817088a2a236a2683ba597baa
GabbyMont <gemo1236@gmail.com>
2018-04-11 09:03:57 -0400

    css and confirmation done

--- app.rb
+++ app.rb
@@ -1,37 +1,85 @@
 require 'sinatra'
 require_relative 'pizza_function.rb'
+enable :sessions
 
 get '/' do
-	session[:pizza_results] = Array.new
-	erb :get_name
+	session[:pizza_input] = [] # Creates new array for pizza, toppings & sides
+	session[:total_cost] = 0
+	erb :enter # Going to page "enter"/ page 1
 end
 
-post '/get_name' do
-	redirect '/build_pizza'
+post '/pizza' do
+	redirect '/menu' # Redirects to build_pizza get
 end
 
-get '/build_pizza' do
-	session[:size] = params[:size]
+# clears sessions to prevent runover from other pizza
+get '/menu' do
+	session[:size] = ""
+	session[:crust] = ""
+	session[:sauce] = ""
+	session[:meats] = ""
+	session[:veggies] = ""
+	session[:special_topping] = ""
+	session[:extra] = ""
+	session[:salad_type] = ""
+	session[:pasta_type] = ""
+	session[:drink_choice] = ""
+	session[:choice] = ""
+	erb :build_pizza
+end
+
+post '/pizza_menu' do
+	session[:size] = params[:size] 
 	session[:crust] = params[:crust]
 	session[:sauce] = params[:sauce]
-	session[:meats] = params[:meats]
+	session[:meats] = params[:meats] 
 	session[:veggies] = params[:veggies]
 	session[:special_topping] = params[:special_topping]
 	session[:extra] = params[:extra]
- 	erb :build_pizza, locals:{size:size,crust:crust,sauce:sauce,meats:meats,veggies:veggies,special_topping:special_topping,extra:extra}
+	session[:salad_type] = params[:salad_type]
+	session[:pasta_type] = params[:pasta_type]
+	session[:drink_choice] = params[:drink_choice]
+	session[:choice] = params[:choice]
+
+	if session[:choice] == "More"
+		pizza_cost = cost(session[:size])
+		session[:total_cost] += pizza_cost
+
+		session[:pizza_input] << [session[:size], session[:crust], session[:sauce], session[:meats], session[:veggies], session[:special_topping], session[:extra], session[:salad_type], session[:pasta_type], session[:drink_choice]]
+		redirect '/menu'
+	else session[:choice] == "Next"
+		pizza_cost = cost(session[:size])
+		session[:total_cost] += pizza_cost
+
+		session[:pizza_input] << [session[:size], session[:crust], session[:sauce], session[:meats], session[:veggies], session[:special_topping], session[:extra], session[:salad_type], session[:pasta_type], session[:drink_choice]]
+		redirect '/delivery'
+	end
 end
 
-post '/build_pizza' do
-	redirect '/wings'
+get '/delivery' do
+	erb :delivery
 end
 
-get '/wings' do
+post '/delivery_input' do
+	address = params[:address]
+	delivery_name = params[:name]
+	session[:total_cost] += 5.00
+	session[:address] = "#{address} is the address of delivery."
+	session[:name] = "#{delivery_name} is the name of the order."
+	redirect '/final'
 end
 
-# post '/pizza_input' do
-# 	redirect '/results'
-# end
+post '/no_delivery' do
+	delivery_name = params[:name]
+	session[:name] = "#{delivery_name} is the name of the order."
+	session[:address] = ""
+	redirect '/final'
+end
 
-# get '/results' do
-# 	erb :results
-# end
+get '/final' do
+	erb :final_page
+end
+
+post '/reset' do
+	redirect '/'
+end
